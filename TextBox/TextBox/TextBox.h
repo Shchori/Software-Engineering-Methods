5766b77bd9b1caa7685056fe20749ba09fdadf61
Shchori <nir.shc@gmail.com>
2016-03-18 09:11:20 +0200

    update the new TextBox

---
+++ TextBox/TextBox/TextBox.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#include "ConsolComponent.h"
+#define FRAME_SIZE 2;
+using namespace std; 
+
+class TextBox : public ConsolComponent
+{
+public:
+	TextBox(const int size ,const COORD coord, HANDLE consol, string context);
+	bool inArea(COORD c);
+	int mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output);
+	int keyPress(KEY_EVENT_RECORD ker, HANDLE output);
+	~TextBox();
+private:
+	int size;
+	string buffer;
+	COORD c;
+	HANDLE consol;
+};
+
