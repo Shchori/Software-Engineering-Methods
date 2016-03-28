4286be39a5891fef1095617e360ed65e43182e78
Shchori <nir.shc@gmail.com>
2016-03-28 22:05:03 +0300

    1

--- TextBox/TextBox/TextBox.h
+++ TextBox/TextBox/TextBox.h
@@ -11,12 +11,13 @@ public:
 	TextBox(const int size ,const COORD coord, HANDLE consol, string context);
 	bool inArea(COORD c);
 	int mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output);
-	int keyPress(KEY_EVENT_RECORD ker, HANDLE output);
+	int keyPress(KEY_EVENT_RECORD ker, HANDLE output,COORD);
 	~TextBox();
 private:
 	int size;
 	string buffer;
 	COORD c;
 	HANDLE consol;
+	void refresh();
 };
 
