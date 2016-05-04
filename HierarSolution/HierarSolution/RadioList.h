44bcd9894fbc345e37c7a1d42c4972cfa7ad4178
Sapir Abargil <sapira592@gmail.com>
2016-05-04 22:08:41 +0300

    radioList

---
+++ HierarSolution/HierarSolution/RadioList.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "ResponseComponentCompositor.h"
+#include <Windows.h>
+#include <string>
+#define FRAME_SIZE 2;
+using namespace std;
+
+class RadioList: public ResponseComponentCompositor
+{
+public:
+	RadioList(const int size, const COORD coord, string context[]);
+	bool inArea(COORD c);
+	int mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output);
+	int keyPress(KEY_EVENT_RECORD ker, HANDLE output, COORD);
+	~RadioList();
+private:
+	int size;
+	int length;
+	string *buffer;
+	int current;
+	int mark;
+	void print();
+	void mark(int n);
+};
+
--- HierarSolution/HierarSolution/ResponseComponentCompositor.h
+++ HierarSolution/HierarSolution/ResponseComponentCompositor.h
@@ -3,8 +3,10 @@
 
 class ResponseComponentCompositor:public ResponseComponent
 {
-private:
-public:
+protected:
 	virtual void mark(int n)=0;
+public:
+	ResponseComponentCompositor(const COORD c) :ResponseComponent(c) {};
+	
 	virtual ~ResponseComponentCompositor ()= 0;
 };
