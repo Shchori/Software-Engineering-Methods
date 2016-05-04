0b6a3eb8d7b59a6ddc918fa6d9979bef7962d3b7
Sapir Abargil <sapira592@gmail.com>
2016-05-04 22:21:34 +0300

    change combox

--- HierarSolution/HierarSolution/ComboBox.cpp
+++ HierarSolution/HierarSolution/ComboBox.cpp
@@ -1,4 +1,4 @@
-#include "RadioList.h"
+#include "ComboBox.h"
 #include <cstdio>
 #include <string>
 using namespace std;
@@ -7,7 +7,7 @@ using namespace std;
 
 
 
-RadioList::RadioList(const int size, const COORD coord, string context[]) : ResponseComponentCompositor(coord), size(size)
+ComboBox::ComboBox(const int size, const COORD coord, string context[]) : ResponseComponentCompositor(coord), size(size)
 {
 	this->buffer = context;
 	this->_mark = 1;
@@ -16,42 +16,46 @@ RadioList::RadioList(const int size, const COORD coord, string context[]) : Resp
 	for (int i = 0; i < size; ++i) {
 		if (this->length < context[i].length()) this->length = context[i].length();
 	}
-	this->length += 3;
+	this->mark = 0;
 	this->current = 0;
 	print();
 }
 bool beetween(int p, int x1, int x2) {
 	return (p >= x1&&p <= x2);
 }
-bool RadioList::inArea(COORD c)
+bool ComboBox::inArea(COORD c)
 {
 	int x = GetCoord().X, y = GetCoord().Y;
-	return beetween(c.X, x, x + this->length + 5) && beetween(c.Y, y, y + size + 2);
+	return beetween(c.X, x, x + this->length + 2) && beetween(c.Y, y, y + size + 4);
 }
 
-int RadioList::mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output)
+int ComboBox::mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output)
 {
 	COORD c = mer.dwMousePosition;
 	if (inArea(c)) {
-		if (beetween(c.Y, GetCoord().Y + 1, GetCoord().Y + this->size)) {
-			this->current = c.Y - GetCoord().Y - 1;
+		if (beetween(c.X, GetCoord().Y, GetCoord().Y + 3)) {
+			this->current = 1;
+		}
+		else {
+			this->current = c.Y - GetCoord().Y - 3;
+			_mark = current;
 		}
 		this->print();
 	}
 	return 0;
 }
 
-int RadioList::keyPress(KEY_EVENT_RECORD ker, HANDLE output, COORD c)
+int ComboBox::keyPress(KEY_EVENT_RECORD ker, HANDLE output, COORD c)
 {
 	int pos = c.X - (GetCoord().X + 1);
 	COORD temp = c;
 	switch (ker.wVirtualKeyCode) {
 	case VK_UP:
-		if (current > 0) this->current -= 1;
+		if (current > 0) this->mark -= 1;
 		print();
 		break;
 	case VK_DOWN:
-		if (current < this->size - 1) this->current += 1;
+		if (current < this->size) this->mark += 1;
 		print();
 		break;
 	case VK_RETURN:
@@ -64,7 +68,7 @@ int RadioList::keyPress(KEY_EVENT_RECORD ker, HANDLE output, COORD c)
 }
 
 
-void RadioList::print() {
+void ComboBox::print() {
 	COORD temp = GetCoord();
 	SetConsoleCursorPosition(GetConsole(), temp);
 	DWORD wAttr = FOREGROUND_BLUE | FOREGROUND_INTENSITY;
@@ -74,30 +78,39 @@ void RadioList::print() {
 	DWORD wAttr2 = cbi.wAttributes | BACKGROUND_RED;
 	SetConsoleTextAttribute(GetConsole(), wAttr2);
 	SetConsoleCursorPosition(GetConsole(), temp);
+	printf("%c", 0xC9);
+	printf("%s", string(this->length, 0xCD).c_str());
+	printf("%c", 0xBB);
+	temp.Y += 1;
+	SetConsoleCursorPosition(GetConsole(), temp);
+	printf("%c", 0xBA);
+	printf(buffer[this->current].data());
+	printf("%s", string(this->length - buffer[current].length(), ' ').c_str());
+	printf("%c", 0xBA);
+	temp.Y += 1;
+	SetConsoleCursorPosition(GetConsole(), temp);
 	printf("%c", 0xB9);
-	printf("%s", string(this->length + 3, 0xCD).c_str());
+	printf("%s", string(this->length, 0xCD).c_str());
 	printf("%c", 0xCC);
 	temp.Y += 1;
 	SetConsoleCursorPosition(GetConsole(), temp);
 	for (int i = 0; i < size; i++)
 	{
-		if (i == current) {
-			string tempS = "(X)" + buffer[i];
+		if (i == _mark) {
 			CONSOLE_SCREEN_BUFFER_INFO cbi;
 			GetConsoleScreenBufferInfo(GetConsole(), &cbi);
 			DWORD temp = cbi.wAttributes;
 			DWORD wAttr = FOREGROUND_INTENSITY | BACKGROUND_GREEN;
 			SetConsoleTextAttribute(GetConsole(), wAttr);
 			printf("%c", 0xBA);
-			printf(tempS.data());
+			printf(buffer[i].data());
 			printf("%s", string(this->length - buffer[i].length(), ' ').c_str());
 			printf("%c", 0xBA);
 			SetConsoleTextAttribute(GetConsole(), temp);
 		}
 		else {
 			printf("%c", 0xBA);
-			string tempS = "( )" + buffer[i];
-			printf(tempS.data());
+			printf(buffer[i].data());
 			printf("%s", string(this->length - buffer[i].length(), ' ').c_str());
 			printf("%c", 0xBA);
 		}
@@ -107,16 +120,9 @@ void RadioList::print() {
 	}
 
 	printf("%c", 0xC8);
-	printf("%s", string(this->length + 3, 0xCD).c_str());
+	printf("%s", string(this->length, 0xCD).c_str());
 	printf("%c", 0xBC);
 	SetConsoleCursorPosition(GetConsole(), GetCoord());
 }
 
-RadioList::~RadioList()
-{
-}
-
-
-
-
-
+ComboBox::~ComboBox() {}
--- HierarSolution/HierarSolution/ComboBox.h
+++ HierarSolution/HierarSolution/ComboBox.h
@@ -5,21 +5,22 @@
 #define FRAME_SIZE 2;
 using namespace std;
 
-class RadioList: public ResponseComponentCompositor
-{
+class ComboBox : public ResponseComponentCompositor{
+
 public:
-	RadioList(const int size, const COORD coord, string context[]);
+	ComboBox(const int size, const COORD coord, string context[]);
 	bool inArea(COORD c);
 	int mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output);
 	int keyPress(KEY_EVENT_RECORD ker, HANDLE output, COORD);
-	~RadioList();
+	~ComboBox();
+
 private:
 	int size;
 	int length;
 	string *buffer;
 	int current;
 	int _mark;
-	void print();
 	void mark(int n);
+	void print();
 };
 
