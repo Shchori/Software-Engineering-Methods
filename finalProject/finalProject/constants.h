02db73f44171ae7fc4e63527408f7bcc8662d127
Shchori <nir.shc@gmail.com>
2016-05-25 17:12:07 +0300

    Icontrol interface

---
+++ finalProject/finalProject/constants.h
@@ -0,0 +1,4 @@
+#pragma once
+enum class BorderType { Single, Double, None };
+enum class ForegroundColor { Red, Blue, Green, Purple, Teal, Yellow, White, Black };
+enum class BackgroundColor { Red, Blue, Green, Purple, Teal, Yellow, White, Black };
