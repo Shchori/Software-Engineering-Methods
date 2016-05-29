8a125d5db9f859ea368b288839c1148c8d1b16f4
ZOHAR <tz.zohar@gmail.com>
2016-05-29 08:58:59 +0300

    PartAZohar

---
+++ Zohar/Zohar/Main.cpp
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <windows.h>
+#include "RadioList.h"
+
+using namespace std;
+
+HANDLE hStdin;
+DWORD fdwSaveOldMode;
+
+VOID ErrorExit(LPSTR);
+VOID KeyEventProc(KEY_EVENT_RECORD, HANDLE, RadioList&);
+VOID MouseEventProc(MOUSE_EVENT_RECORD, HANDLE, RadioList&);
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
+
+int main(VOID)
+{
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci = { 100, FALSE }; // invisibil cursor
+	SetConsoleCursorInfo(console, &cci);;
+
+	DWORD cNumRead, fdwMode, i;
+	INPUT_RECORD irInBuf[128];
+	int counter = 0;
+
+	// Get the standard input handle. 
+
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	if (hStdin == INVALID_HANDLE_VALUE)
+		ErrorExit("GetStdHandle");
+
+	// Save the current input mode, to be restored on exit. 
+
+	if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
+		ErrorExit("GetConsoleMode");
+
+	// Enable the window and mouse input events. 
+
+	fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+	if (!SetConsoleMode(hStdin, fdwMode))
+		ErrorExit("SetConsoleMode");
+
+	// Loop to read and handle the next 100 input events.
+	COORD c = { 7, 7 };
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	string buffer[] = { "1234","12345","123456","1234567","123456789"};
+	RadioList box(5, c, buffer);
+	while (true)
+	{
+		// Wait for the events. 
+
+		if (!ReadConsoleInput(
+			hStdin,      // input buffer handle 
+			irInBuf,     // buffer to read into 
+			128,         // size of read buffer 
+			&cNumRead)) // number of records read 
+			ErrorExit("ReadConsoleInput");
+
+		// Dispatch the events to the appropriate handler. 
+
+		for (i = 0; i < cNumRead; i++)
+		{
+			switch (irInBuf[i].EventType)
+			{
+			case KEY_EVENT: // keyboard input 
+				KeyEventProc(irInBuf[i].Event.KeyEvent, h, box);
+				break;
+
+			case MOUSE_EVENT: // mouse input 
+				MouseEventProc(irInBuf[i].Event.MouseEvent, h, box);
+				break;
+
+			case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing 
+										   //ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+				break;
+
+			case FOCUS_EVENT:  // disregard focus events 
+
+			case MENU_EVENT:   // disregard menu events 
+				break;
+
+			default:
+				ErrorExit("Unknown event type");
+				break;
+			}
+		}
+	}
+
+	// Restore input mode on exit.
+
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+
+	return 0;
+}
+
+VOID ErrorExit(LPSTR lpszMessage)
+{
+	fprintf(stderr, "%s\n", lpszMessage);
+
+	// Restore input mode on exit.
+
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+
+	ExitProcess(0);
+}
+
+VOID KeyEventProc(KEY_EVENT_RECORD ker, HANDLE h, RadioList& c)
+{
+
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(h, &info);
+	if (ker.bKeyDown) {}
+	else if (c.inArea(info.dwCursorPosition)) {
+		c.keyPress(ker, h, info.dwCursorPosition);
+	}
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer, HANDLE h, RadioList& c)
+{
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+	//printf("Mouse event: ");
+
+	switch (mer.dwEventFlags)
+	{
+	case 0:
+
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		{
+			if (c.inArea(mer.dwMousePosition)) {
+				c.mouseEvent(mer, h);
+			}
+		}
+		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+		{
+			//printf("right button press \n");
+		}
+		else
+		{
+			//printf("button press\n");
+		}
+		break;
+	case DOUBLE_CLICK:
+		//printf("double click\n");
+		break;
+	case MOUSE_HWHEELED:
+		//printf("horizontal mouse wheel\n");
+		break;
+	case MOUSE_MOVED:
+		//printf("mouse moved\n");
+		break;
+	case MOUSE_WHEELED:
+		//printf("vertical mouse wheel\n");
+		break;
+	default:
+		//printf("unknown\n");
+		break;
+	}
+}
+
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+	//printf("Resize event\n");
+	//printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
+}
