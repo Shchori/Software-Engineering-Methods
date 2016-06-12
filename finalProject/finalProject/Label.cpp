eaa53fb35b8ac7bf46cdc847ed9c7c01be2cb510
Sapir Abargil <sapira592@gmail.com>
2016-06-12 14:38:24 +0300

    fixed label

--- finalProject/finalProject/Label.cpp
+++ finalProject/finalProject/Label.cpp
@@ -1,8 +1,7 @@
 #include "Label.h"
 
 
-
-Label::Label(string s, int width):IControl(3, width+2, BorderType::None, Color::White, Color::Black, false, true, false),str("")
+Label::Label(string s, int width) :IControl(3, width + 2, BorderType::None, false, true, false, Color::White, Color::Black), str("")
 {
 	this->SetValue(s);
 
@@ -14,7 +13,7 @@ Label::~Label()
 
 void Label::draw()
 {
-	if (this->getVisability()&&this->isCoordSet()) {
+	if (this->getVisability() && this->_isCoordSet()) {
 		IControl::draw();
 		Graphics g = Graphics::getInstance();
 		g.write(this->getCoord().X + 1, this->getCoord().Y + 1, str);
