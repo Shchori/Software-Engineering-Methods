ed08a819cb5c6443c9675033b3f085e52cb7f29c
Avital <avitalg91@gmail.com>
2016-06-12 21:22:41 +0300

    radio- start

--- finalProject/finalProject/RadioList.cpp
+++ finalProject/finalProject/RadioList.cpp
@@ -1,7 +1,7 @@
 #include "RadioList.h"
 
 
-RadioList::RadioList(int height, int width, vector<string> options):IControl(height, width, BorderType::None, Color::White, Color::Black, false, true, false), selectedIndex(-1)
+RadioList::RadioList(int height, int width, vector<string> options):IControl(height, width),selectedIndex(-1)
 {
 	this->options = options;
 }
@@ -10,8 +10,8 @@ void RadioList::draw() {
 	Graphics graphics = Graphics::getInstance();
 	Border* border=NULL;
 	
-	if (visability) {
-		switch (borderType) {
+	if (getVisability()) {
+		switch (getBorder()) {
 		case BorderType::Single:
 			break;
 		case BorderType::Double:
