1a7fe27a35516f9168f459c7b594f489b8cd8b28
ZOHAR <tz.zohar@gmail.com>
2016-06-15 22:45:43 +0300

    maincng

--- finalProject/finalProject/main.cpp
+++ finalProject/finalProject/main.cpp
@@ -10,9 +10,48 @@
 #include "Panel.h"
 #include "TextBox.h"
 #include <iostream>
+#include "Button.h"
+#include "NumericBox.h"
 using namespace std;
 void main() {
 
+	/*BorderType b = BorderType::Double;
+	Label t("jj", 4);
+	COORD f = { 15, 17 };
+	t.setCoord(f);
+	t.setBorder(b);
+
+	Label text("hey", 4);
+	COORD c = { 2,3 };
+	text.setCoord(c);
+	text.setBorder(b);
+
+
+	Panel main(25, 30);
+	main.setBorder(b);
+	main.AddControl(t, t.getCoord().X, t.getCoord().Y);
+	main.AddControl(text, text.getCoord().X, text.getCoord().Y);
+	//main.draw();
+
+	string plus = "plus";
+	string minus = "minus";
+	COORD numeric_c = { 6,7 };
+	NumericBox numericbox(10, 20, 0, 100, 1, 4, numeric_c);
+	numericbox.setBorder(b);
+	main.AddControl(numericbox, numericbox.getCoord().X, numericbox.getCoord().Y);
+
+	numericbox.draw();
+
+	main.draw();
+
+
+	numericbox.addEvent(plus);
+	numericbox.addEvent(plus);
+	numericbox.addEvent(minus);
+
+
+	//getchar();
+	*/
 	HANDLE hStdin;
 	DWORD fdwSaveOldMode;
 
@@ -98,7 +137,7 @@ void main() {
 	// Restore input mode on exit.
 
 	SetConsoleMode(hStdin, fdwSaveOldMode);
-
+	
 	getchar();
 
 }
