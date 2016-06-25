80948437b48e260909209681c1ee95386f96cec7
Shchori <nir.shc@gmail.com>
2016-06-25 14:53:46 +0300

    radiolist

--- finalProject/finalProject/RadioList.cpp
+++ finalProject/finalProject/RadioList.cpp
@@ -1,24 +1,24 @@
 #include "RadioList.h"
 
 
-RadioList::RadioList(int height, int width, vector<string> options):Panel(height, width),selectedIndex(-1),ops(options.size(),Label(width-5,"")), btn(options.size(), RadioListButton(this))
+RadioList::RadioList(int height, int width, vector<string> options) :Panel(height, width), selectedIndex(-1), ops(options.size(), Label(width - 5, "")), btn(options.size(), RadioListButton(this))
 {
-	for (int i = 0; i < options.size();++i) {
-		COORD c1 = { 0, i*1 };
-		COORD c2 = { 6, i*1 };
+	for (int i = 0; i < options.size(); ++i) {
+		COORD c1 = { 0, i * 1 };
+		COORD c2 = { 6, i * 1 };
 		ops[i].setValue(options[i]);
 		ops[i].setCoord(c2);
 		btn[i].setCoord(c1);
-			addControl(btn[i], btn[i].getCoord().X, btn[i].getCoord().Y);
-			addControl(ops[i], ops[i].getCoord().X, ops[i].getCoord().Y);
+		addControl(btn[i], btn[i].getCoord().X, btn[i].getCoord().Y);
+		addControl(ops[i], ops[i].getCoord().X, ops[i].getCoord().Y);
 	}
-	
+
 }
 
 void RadioList::draw() {
 
 	Panel::draw();
-	
+
 	g.setCursorVisibility(false);
 }
 
@@ -39,10 +39,10 @@ int RadioList::mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output) {
 
 		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
 		{
-			if (getCoord().X<mer.dwMousePosition.X&& mer.dwMousePosition.X<=getCoord().X+3) {
+			if (getCoord().X<mer.dwMousePosition.X&& mer.dwMousePosition.X <= getCoord().X + 3) {
 				for (int i = 0; i < btn.size(); i++) {
 					if (getCoord().Y + i <= mer.dwMousePosition.Y && mer.dwMousePosition.Y < getCoord().Y + i + 1) {
-						
+
 						if (selectedIndex == -1) {
 							selectedIndex = i;
 						}
@@ -54,7 +54,7 @@ int RadioList::mouseEvent(MOUSE_EVENT_RECORD mer, HANDLE output) {
 					}
 				}
 			}
-			
+
 		}
 	}
 	return 0;
