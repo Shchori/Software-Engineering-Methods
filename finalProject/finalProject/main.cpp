fddb6a2760b4fecf42b4817400d7be8bf0476e79
Avital <avitalg91@gmail.com>
2016-07-08 18:47:07 +0300

    MAIN

--- finalProject/finalProject/main.cpp
+++ finalProject/finalProject/main.cpp
@@ -15,13 +15,7 @@ private:
 };
 int main(int argc, char **argv)
 {
-	vector<string> str = { "hello", "you" };
-	RadioList r(20, 20, str);
-	Panel main(40, 40);
-	main.addControl(r, 2, 2);
-	EventEngine engine;
-	engine.run(main);
-	/*Label lName(20);
+	Label lName(20);
 	lName.setValue("Name: ");
 
 	Label lAddress(20);
@@ -74,7 +68,7 @@ int main(int argc, char **argv)
 	main.addControl(bSubmit, 1, 22);
 	Control::setFocused(&tName);
 	EventEngine engine;
-	engine.run(main);*/
+	engine.run(main);
 	return 0;
 }
 
